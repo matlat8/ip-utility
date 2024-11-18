eb46b7f2aa92f6ec4a67ce2a28f7ad9f4e2be661
Matthew Latham <50916823+matlat8@users.noreply.github.com>
2024-11-17 21:50:01 -0500

    chore: Update update.sh script to include iputility.service and enable it

--- update.sh
+++ update.sh
@@ -7,3 +7,13 @@ go build main.go
 mkdir -p /etc/iputility
 
 cp main /etc/iputility/iputility
+
+cp iputility.service /etc/systemd/system/iputility.service
+
+systemctl daemon-reload
+
+systemctl restart iputility
+
+systemctl enable iputility
+
+echo "Update complete"
